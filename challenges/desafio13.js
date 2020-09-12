36d4037bee5530ef28f9809196e06f411094b76b
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-09-12 16:41:19 -0300

    Finalizando desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: { 
+      $elemMatch: { 
+        $and: [
+          { tipo: "sódio" },
+          { percentual: { $gte: 40 } }
+        ] 
+      } 
+    }
+  },
+  { 
+    $push: { tags: "muito sódio" } 
+  }
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, tags: 1 }
+);
